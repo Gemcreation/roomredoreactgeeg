b180b57cb12531efff7f9c0770645ee31770a3d9
Gem <mojeedibrahim13@gmail.com>
2025-11-08 08:13:41 -0800

    header added

---
+++ src/components/Header.jsx
@@ -0,0 +1,23 @@
+import roomlogo from "../assets/images/logo.svg";
+
+const Header = () =>{
+
+    return(
+        <header>
+            <a href="/">
+                <img className="site-header" src={roomlogo} alt="Room logo" />
+            </a>
+
+            <nav>
+                <ul>
+                    <li><a href="#home">home</a></li>
+                    <li><a href="#shop">shop</a></li>
+                    <li><a href="#about">about</a></li>
+                    <li><a href="#contact">contact</a></li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default Header;
