0a265e874a4d9c565263f9e66acb48d66e2b0287
Gem <mojeedibrahim13@gmail.com>
2025-11-18 02:48:25 -0800

    resizing

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,23 @@
 import roomlogo from "../assets/images/logo.svg";
 import iconHamburgerOpen from "../assets/images/icon-hamburger.svg";
 import iconHamburgerClose from "../assets/images/icon-close.svg";
+import { useState } from "react";
 
 const Header = () =>{
+    const [navDisplay, setNavDisplay] = useState(false);
+    const [deviceWidth, setDeviceWidth] = useState(window.innerWidth);
+  
+    const openNav = () => {
+      setNavDisplay(true);
+    }
+  
+    const closeNav = () => {
+      setNavDisplay(false);
+    }
+  
+    window.onresize = () => {
+      setDeviceWidth(window.innerWidth);
+    }
 
     return(
         <header className="header">
@@ -10,10 +25,10 @@ const Header = () =>{
                 <img className="site-header" src={roomlogo} alt="Room logo" />
             </a>
 
-            <nav>
+            <nav className={(deviceWidth < 600) ? (navDisplay ? "open" : "") : ""}>
 
                 {/* for icon open nav*/}
-                <img className="site-header" src={iconHamburgerOpen} alt="Room logo" />
+                <img className="site-header" src={iconHamburgerOpen} alt="Room logo" id="iconHamburgerOpen" onClick={closeNav} />
                 <ul>
                     <li><a href="#home">home</a></li>
                     <li><a href="#shop">shop</a></li>
@@ -23,7 +38,7 @@ const Header = () =>{
             </nav>
 
             {/* for icon close nav */}
-            <img className="site-header" src={iconHamburgerClose} alt="Room logo" />
+            <img className="site-header" src={iconHamburgerClose} alt="Room logo" onClick={openNav} />
         </header>
     )
 }
